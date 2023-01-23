278ff00084945ce130ed310dfa1b6c0e47259c91
Shashank Srivastava <shashank.srivastava25sks@gmail.com>
2023-01-23 10:56:29 +0530

    Initialised Repository with Coming Soon Page

---
+++ src/components/ComingSoon.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from '../styles/ComingSoon.module.css'
+
+
+const ComingSoon = () => {
+  return (
+    <div className={styles.container}>
+        <h1 className={styles.title}>
+          Apoorv 2k23 | Coming Soon
+        </h1>
+        <p className={styles.description}>
+          IIITK's Annual Techno Cultural Fest
+        </p> 
+      </div>
+  )
+}
+
+export default ComingSoon
