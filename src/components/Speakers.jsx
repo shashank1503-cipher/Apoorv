450d5242eb2d0cc500d3d9afb0df180500c260d8
Shashank Srivastava <shashank.srivastava25sks@gmail.com>
2023-03-13 02:31:23 +0530

    Added Animations

--- src/components/Speakers.jsx
+++ src/components/Speakers.jsx
@@ -10,75 +10,90 @@ import img6 from '../assets/6.png';
 import img7 from '../assets/7.png';
 import img8 from '../assets/8.png';
 // import img2 from "../components/Images/sampleImg.jpg";
-
+import { motion } from 'framer-motion'
 const Speakers = () => {
     const speakersList = [
         {
             image: img1,
-            title:'Naoko Okiyama',
-            description:'A.C.O. by Monsterlab COO / Art Director'
+            title: 'Naoko Okiyama',
+            description: 'A.C.O. by Monsterlab COO / Art Director'
         },
         {
-            image:img2,
-            title:'Naoko Okiyama2',
-            description:'A.C.O. by Monsterlab COO / Art Director'
+            image: img2,
+            title: 'Naoko Okiyama2',
+            description: 'A.C.O. by Monsterlab COO / Art Director'
         },
 
         {
             image: img3,
-            title:'Naoko Okiyama3',
-            description:'A.C.O. by Monsterlab COO / Art Director'
+            title: 'Naoko Okiyama3',
+            description: 'A.C.O. by Monsterlab COO / Art Director'
         },
         {
-            image:img4,
-            title:'Naoko Okiyama4',
-            description:'A.C.O. by Monsterlab COO / Art Director'
+            image: img4,
+            title: 'Naoko Okiyama4',
+            description: 'A.C.O. by Monsterlab COO / Art Director'
         },
 
         {
             image: img5,
-            title:'Naoko Okiyama5',
-            description:'A.C.O. by Monsterlab COO / Art Director'
+            title: 'Naoko Okiyama5',
+            description: 'A.C.O. by Monsterlab COO / Art Director'
         },
         {
-            image:img6,
-            title:'Naoko Okiyama6',
-            description:'A.C.O. by Monsterlab COO / Art Director'
+            image: img6,
+            title: 'Naoko Okiyama6',
+            description: 'A.C.O. by Monsterlab COO / Art Director'
         },
 
         {
             image: img7,
-            title:'Naoko Okiyama7',
-            description:'A.C.O. by Monsterlab COO / Art Director'
+            title: 'Naoko Okiyama7',
+            description: 'A.C.O. by Monsterlab COO / Art Director'
         },
         {
-            image:img8,
-            title:'Naoko Okiyama8',
-            description:'A.C.O. by Monsterlab COO / Art Director'
+            image: img8,
+            title: 'Naoko Okiyama8',
+            description: 'A.C.O. by Monsterlab COO / Art Director'
         }
     ];
 
-  return (
-    <div style={{width:"100%",display:"flex",justifyContent:"center",backgroundColor:"#F2EFEB"}}>
-    <div className={styles.SpeakersContainer}>
-            <div className= {styles.title}>
-                <h1>
-                    Speakers
-                </h1>
+    return (
+        <div style={{ width: "100%", display: "flex", justifyContent: "center", backgroundColor: "#F2EFEB" }}>
+            <div className={styles.SpeakersContainer}>
+                <motion.div className={styles.title} initial="hidden" whileInView="visible" variants={{
+                    hidden: {
+
+                        opacity: 0,
+                        y: 20,
+                    },
+                    visible: {
+
+                        opacity: 1,
+                        y: 0,
+                        transition: {
+                            duration: 0.25,
+
+                        }
+                    },
+                }}>
+                    <h1>
+                        Speakers
+                    </h1>
+                </motion.div>
+
+                <div className={styles.SpeakerCards}>
+
+                    {speakersList.map((item, index) => {
+                        return <SpeakerCard key={item.title} speaker={item} />
+                    })}
+
+
+                </div>
             </div>
-
-        <div className={styles.SpeakerCards}>
-
-            {speakersList.map((item, index) => {
-                return <SpeakerCard  key={item.title} speaker={item} />
-            })}
-
-        
         </div>
-    </div>
-    </div>
-    
-  )
+
+    )
 }
 
 export default Speakers
