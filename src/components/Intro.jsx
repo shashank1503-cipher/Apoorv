b889709939dbc288cd4e174dc5ac7b710c434044
Ishaan Mahesh <78961353+ishaan5199@users.noreply.github.com>
2023-03-19 14:35:21 +0530

    preloader bug fix

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -9,48 +9,56 @@ import im5 from "../../public/static/images/im5.png"
 import im6 from "../../public/static/images/im6.png"
 
 const Intro = () => {
+
+    const [width, setWidth] = useState(1920);
+
+    useEffect(() => {
+        let w = window.innerWidth;
+        setWidth(w);
+    }, []);
+
     return (
         <div className = {styles.container}>
             <div className = {styles.col}>
-                <div className = {styles.slideDown} style = {window.innerWidth <= 468 ? {right: "60%"} : {right: "55%"}}>
+                <div className = {styles.slideDown} style = {width <= 468 ? {right: "60%"} : {right: "55%"}}>
                     <Image src = {im1} 
                     alt = "A"
-                    width = {window.innerWidth <= 468 ? 40 : 100}
-                    height = {window.innerWidth <= 468 ? 40 : 100}/>
+                    width = {width <= 468 ? 40 : 100}
+                    height = {width <= 468 ? 40 : 100}/>
                 </div>
                 <div className = {styles.slideRight} style = {{top: "52%"}}>
                     <Image src = {im4} 
                     alt = "O"
-                    width = {window.innerWidth <= 468 ? 40 : 100}
-                    height = {window.innerWidth <= 468 ? 40 : 100}/>
+                    width = {width <= 468 ? 40 : 100}
+                    height = {width <= 468 ? 40 : 100}/>
                 </div>
             </div>
             <div className = {styles.col}>
                 <div className = {styles.fadeIn} style = {{bottom: "52%"}}>
                     <Image src = {im2} 
                     alt = "P"
-                    width = {window.innerWidth <= 468 ? 40 : 100}
-                    height = {window.innerWidth <= 468 ? 40 : 100}/>
+                    width = {width <= 468 ? 40 : 100}
+                    height = {width <= 468 ? 40 : 100}/>
                 </div>
                 <div className = {styles.fadeIn} style = {{top: "52%"}}>
                     <Image src = {im5} 
                     alt = "R"
-                    width = {window.innerWidth <= 468 ? 40 : 100}
-                    height = {window.innerWidth <= 468 ? 40 : 100}/>
+                    width = {width <= 468 ? 40 : 100}
+                    height = {width <= 468 ? 40 : 100}/>
                 </div>
             </div>
             <div className = {styles.col}>
-                <div className = {styles.slideLeft} style = {window.innerWidth <= 468 ? {bottom: "52%", left: "60%"} : {bottom: "52%", left: "55%"}}>
+                <div className = {styles.slideLeft} style = {width <= 468 ? {bottom: "52%", left: "60%"} : {bottom: "52%", left: "55%"}}>
                     <Image src = {im3} 
                     alt = "O"
-                    width = {window.innerWidth <= 468 ? 40 : 100}
-                    height = {window.innerWidth <= 468 ? 40 : 100}/>
+                    width = {width <= 468 ? 40 : 100}
+                    height = {width <= 468 ? 40 : 100}/>
                 </div>
-                <div className = {styles.slideUp} style = {window.innerWidth <= 468 ? {top: "52%", left: "60%"} : {top: "52%", left: "55%"}}>
+                <div className = {styles.slideUp} style = {width <= 468 ? {top: "52%", left: "60%"} : {top: "52%", left: "55%"}}>
                     <Image src = {im6} 
                     alt = "V"
-                    width = {window.innerWidth <= 468 ? 40 : 100}
-                    height = {window.innerWidth <= 468 ? 40 : 100}/>
+                    width = {width <= 468 ? 40 : 100}
+                    height = {width <= 468 ? 40 : 100}/>
                 </div>
             </div>
         </div>
