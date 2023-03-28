faba8883e74429aee3ed86dbd7bb8d519f9effe2
bhavyag54 <bhavyagoyal54@gmail.com>
2023-03-28 18:55:38 +0530

    added teams data, changed speakers data

--- src/data/SpeakerData.jsx
+++ src/data/SpeakerData.jsx
@@ -2,7 +2,7 @@ const speakersList = [
     {
         image: 'https://res.cloudinary.com/dpjf6btln/image/upload/v1679294025/priyansh-agarwal_xgoiv1.png',
         title: 'Priyansh Agarwal',
-        description: 'Founder - TLE Eliminators | ICPC Asia West Finalist | Former SWE Intern @Goldman Sachs | AIR 1 - Google Kick Start | Master @Codeforces | IIIT Delhi\'23'
+        description: 'Founder - TLE Eliminators | ICPC World Finalist | Former SWE Intern @Goldman Sachs | AIR 1 - Google Kick Start | Master @Codeforces | IIIT Delhi\'23'
     },
     {
         image: 'https://res.cloudinary.com/dpjf6btln/image/upload/v1679294024/vikas-kulkarni_evf9in.png',
